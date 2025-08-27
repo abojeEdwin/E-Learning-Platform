4887cbce2cb9a00c3d63045b58f097183fc33fe6
Aboje Edwin <abojeedwin@gmail.com>
2025-08-27 16:20:50 +0100

    Added redis dependency

---
+++ src/main/java/com/elearningplatform/ElearningPlatform.java
@@ -0,0 +1,9 @@
+package com.elearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.elearningplatform")
+public class ElearningPlatform {
+    public static void main(String[] args) {SpringApplication.run(ElearningPlatform.class, args);}
+}
