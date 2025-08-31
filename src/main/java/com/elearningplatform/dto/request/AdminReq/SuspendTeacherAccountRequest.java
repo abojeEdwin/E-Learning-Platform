6cec9a667349bd02e975246af6964152b588edbc
Aboje Edwin <abojeedwin@gmail.com>
2025-08-31 15:34:32 +0100

    Admin service

--- src/main/java/com/elearningplatform/dto/request/AdminReq/SuspendTeacherAccountRequest.java
+++ src/main/java/com/elearningplatform/dto/request/AdminReq/SuspendTeacherAccountRequest.java
@@ -1,13 +1,17 @@
 package com.elearningplatform.dto.request.AdminReq;
 
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class DeleteClientAccountRequest {
+public class SuspendTeacherAccountRequest {
 
     @NotNull(message = "This field is required")
-    private Long clientId;
+    private String reason;
+    @NotNull(message = "This field is required")
+    private Long teacherId;
+
 }
--- src/main/java/com/elearningplatform/service/AdminService.java
+++ src/main/java/com/elearningplatform/service/AdminService.java
@@ -1,9 +1,8 @@
 package com.elearningplatform.service;
 
 import com.elearningplatform.dto.request.AdminReq.CreateAdminRequest;
-import com.elearningplatform.dto.request.AdminReq.DeleteClientAccountRequest;
 import com.elearningplatform.dto.request.AdminReq.LoginAdminRequest;
-import com.elearningplatform.dto.request.AdminReq.ReactivateClientAccountRequest;
+import com.elearningplatform.dto.request.AdminReq.SuspendTeacherAccountRequest;
 import com.elearningplatform.dto.request.ClientReq.SuspendClientAccountRequest;
 import com.elearningplatform.dto.response.ApiResponse;
 import com.elearningplatform.dto.response.AdminRes.CreateAdminResponse;
@@ -14,9 +13,9 @@ public interface AdminService {
     CreateAdminResponse createAdmin(CreateAdminRequest request);
     LoginAdminResponse login(LoginAdminRequest request);
     ApiResponse suspendClientAccount(SuspendClientAccountRequest request);
-    ApiResponse reactivateClientAccount(ReactivateClientAccountRequest request);
-    ApiResponse deleteClientAccount(DeleteClientAccountRequest request);
-    void suspendTeacherAccount(SuspendTeacherAccountRequest request);
-    void reactivateTeacherAccount(ReactivateTeacherAccountRequest request);
-    void deleteTeacherAccount(DeleteTeacherAccountRequest request);
+    ApiResponse reactivateClientAccount(Long clientId);
+    ApiResponse deleteClientAccount(Long clientId);
+    ApiResponse suspendTeacherAccount(SuspendTeacherAccountRequest request);
+    ApiResponse reactivateTeacherAccount(Long teacherId);
+    void deleteTeacherAccount(Long teacherId);
 }
